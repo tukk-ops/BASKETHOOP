f7a6b8592847890c98d69af8d2f056f707d1eac8
tukk-ops <guancongdu@gmail.com>
2026-05-19 22:41:07 +0800

    fix: bind host to 0.0.0.0 and dynamic port

--- app.py
+++ app.py
@@ -141,6 +141,5 @@ def export_csv():
     )
 
 if __name__ == '__main__':
-    app.run(debug=True)
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, debug=True)
